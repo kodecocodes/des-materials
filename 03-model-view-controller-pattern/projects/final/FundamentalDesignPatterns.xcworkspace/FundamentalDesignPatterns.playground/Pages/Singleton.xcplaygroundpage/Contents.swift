494842b799a2fb65139c464809f132f7a99ac402
Deanne Chance <pdxchance@icloud.com>
2022-07-26 06:25:08 -0500

    Add Singleton Pattern Example

--- 03-model-view-controller-pattern/projects/final/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
+++ 03-model-view-controller-pattern/projects/final/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
@@ -11,3 +11,36 @@
  
  ## Code Example
  */
+import UIKit
+
+// Singleton pattern, you cannot create more than one instance
+let app = UIApplication.shared
+//let app2 = UIApplication()
+
+public class MySingleton {
+    
+    static let shared = MySingleton()
+    
+    private init() {}
+    
+}
+
+public class MySingletonPlus {
+
+  static let shared = MySingletonPlus()
+
+  public init() { }
+}
+
+
+let singletonPlus = MySingletonPlus.shared
+let singletonPlus2 = MySingletonPlus()
+
+
+let mySingleton = MySingleton.shared
+//let singleton2 = MySingleton()
+
+
+// Proof of a Singleton Plus patern
+let defaultFileManager = FileManager.default
+let customFileManger = FileManager()
